27a446c8f620527e5ded8145bb49b262a8b665ed
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-06-28 19:31:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-hr = hour();
-mn = minute();
-sc = second();
+let h = hour();
+text('Current hour:\n' + h, 5, 50);
+var hr = hour();
+var mn = minute();
+var sc = second();
 function preload()
 {
 	
@@ -30,10 +32,18 @@ function draw() {
   background(0);
   angleMode(DEGREES);
   scAngle = map(sc, 0, 60, 0, 360);
+  mnAngle = map(mn, 0, 60, 0, 360);
+  hrAngle = map(hr, 0, 60, 0, 360);
   rotate(mnAngle);
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
+  push()
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop()
   drawSprites();
  
 }
