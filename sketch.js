e8fae2c7b19f97ce41b8129a898cdfe2381d10a0
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-06-29 09:57:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(400, 400);
 
 
 	engine = Engine.create();
@@ -33,14 +33,40 @@ function draw() {
   angleMode(DEGREES);
   scAngle = map(sc, 0, 60, 0, 360);
   mnAngle = map(mn, 0, 60, 0, 360);
-  hrAngle = map(hr, 0, 60, 0, 360);
+  hrAngle = map(hr, 0, 12, 0, 360);
+let s = second();
+text('Current second: \n' + s, 200, 50);
+let m = minute();
+text('Current minute: \n' + m, 100, 50);
+let h = hour();
+text('Current hour:\n' + h, 5, 50);
  
   push()
+  translate(200,200)
+  rotate(-90)
   rotate(scAngle);
+  stroke("blue");
+  strokeWeight(3);
+  line(0,0,100,0);
+  pop()
+  push()
+  translate(200,200)
+  rotate(-90)
+  rotate(mnAngle);
   stroke(255,0,0);
   strokeWeight(7);
-  line(700,800,50,0);
+  line(0,0,100,0);
   pop()
+  push()
+  translate(200,200)
+  rotate(-90)
+  rotate(hrAngle);
+  stroke("green");
+  strokeWeight(7);
+  line(0,0,70,0);
+  pop()
+  
+  
   hr = hour();
   mn = minute();
   sc = second();
